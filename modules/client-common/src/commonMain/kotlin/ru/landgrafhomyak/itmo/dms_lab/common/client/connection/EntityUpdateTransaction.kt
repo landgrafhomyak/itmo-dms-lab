e551d42dd99f1fefec5f7a3dd949d5aba47e759e
Andrew Golovashevich <tankist.scratch.p@gmail.com>
2024-02-13 23:01:46 +0300

    Cleaning up unused modules

--- modules/client-common/src/commonMain/kotlin/ru/landgrafhomyak/itmo/dms_lab/common/client/connection/EntityUpdateTransaction.kt
+++
@@ -1,9 +0,0 @@
-package ru.landgrafhomyak.itmo.dms_lab.common.client.connection
-
-import ru.landgrafhomyak.itmo.dms_lab.common.common.descriptors.EntityAttributeDescriptor
-import ru.landgrafhomyak.itmo.dms_lab.common.common.descriptors.EntityDescriptor
-
-interface EntityUpdateTransaction : EntityMutator {
-    suspend fun commit()
-    suspend fun rollback()
-}
